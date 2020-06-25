f9b135516cd9bb2a98641ecfb6fb5df5e511bc7a
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-25 09:31:42 -0300

    routes update

--- routes.ts
+++ routes.ts
@@ -1,13 +1,26 @@
 import { ServerRequest } from "https://deno.land/std/http/server.ts";
 
-export default async function routes(req: ServerRequest) {
-  // read audio file e.g: const rain = await Deno.readFile("rain.mp3");
-  const hello = "Hello Word";
+import { IFiles } from "./types.ts";
 
-  if (req.url === "/" ) {
-    return req.respond({ body: hello, status: 200 });
+export default async function routes(req: ServerRequest, sounds: IFiles) {
+  if (req.url === "/rain" || req.url === "/rain/") {
+    return req.respond({ body: sounds.rain, status: 200 });
   }
-  else {
-    return req.respond({ body: "ERROR 404, PAGE NOT FOUND!", status: 404 });
+  if (req.url === "/water" || req.url === "/water/") {
+    return req.respond({ body: sounds.water, status: 200 });
   }
+  if (req.url === "/storm" || req.url === "/storm/") {
+    return req.respond({ body: sounds.storm, status: 200 });
+  }
+  if (req.url === "/small_waves" || req.url === "/small_waves/") {
+    return req.respond({ body: sounds.small_waves, status: 200 });
+  }
+  if (req.url === "/ocean_waves" || req.url === "/ocean_waves/") {
+    return req.respond({ body: sounds.ocean_waves, status: 200 });
+  }
+  if (req.url === "/forest_ambience" || req.url === "/forest_ambience/") {
+    return req.respond({ body: sounds.forest_ambience, status: 200 });
+  }
+
+  return req.respond({ status: 404 });
 }
