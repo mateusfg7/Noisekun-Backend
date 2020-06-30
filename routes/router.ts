734a9ced07ef5d7991171745521108292d45bee0
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-30 11:00:42 -0300

    'routes' refactor

---
+++ routes/router.ts
@@ -0,0 +1,36 @@
+import deps from "../modules/deps.ts";
+
+import sounds from "../modules/files.ts";
+
+const router = new deps.Router();
+
+router.get("/rain", (context) => {
+  context.response.status = 200;
+  context.response.body = sounds.rain;
+});
+router.get("/water", (context) => {
+  context.response.status = 200;
+  context.response.body = sounds.water;
+});
+router.get("/storm", (context) => {
+  context.response.status = 200;
+  context.response.body = sounds.storm;
+});
+router.get("/small_waves", (context) => {
+  context.response.status = 200;
+  context.response.body = sounds.small_waves;
+});
+router.get("/ocean_waves", (context) => {
+  context.response.status = 200;
+  context.response.body = sounds.ocean_waves;
+});
+router.get("/forest_ambience", (context) => {
+  context.response.status = 200;
+  context.response.body = sounds.forest_ambience;
+});
+router.get("/coffee", (context) => {
+  context.response.status = 200;
+  context.response.body = sounds.coffee;
+});
+
+export default router;
