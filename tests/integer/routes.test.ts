29e8b93ce88666479d994993bddf91bc720cd2a0
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-30 11:15:21 -0300

    test updates

--- tests/integer/routes.test.ts
+++ tests/integer/routes.test.ts
@@ -30,52 +30,78 @@ const routes = {
 
 Deno.test("Route /rain", () => {
   expect(routes["/rain"].status).toBe(200);
+  expect(routes["/rain"].headers.get("content-length")).toBeGreaterThan(0);
 });
 Deno.test("Route /water", () => {
   expect(routes["/water"].status).toBe(200);
+  expect(routes["/water"].headers.get("content-length")).toBeGreaterThan(0);
 });
 Deno.test("Route /storm", () => {
   expect(routes["/storm"].status).toBe(200);
+  expect(routes["/storm"].headers.get("content-length")).toBeGreaterThan(0);
 });
 Deno.test("Route /small_waves", () => {
   expect(routes["/small_waves"].status).toBe(200);
+  expect(routes["/small_waves"].headers.get("content-length")).toBeGreaterThan(
+    0,
+  );
 });
 Deno.test("Route /ocean_waves", () => {
   expect(routes["/ocean_waves"].status).toBe(200);
+  expect(routes["/ocean_waves"].headers.get("content-length")).toBeGreaterThan(
+    0,
+  );
 });
 Deno.test("Route /forest_ambience", () => {
   expect(routes["/forest_ambience"].status).toBe(200);
+  expect(routes["/forest_ambience"].headers.get("content-length"))
+    .toBeGreaterThan(0);
 });
 Deno.test("Route /coffee", () => {
   expect(routes["/coffee"].status).toBe(200);
+  expect(routes["/coffee"].headers.get("content-length")).toBeGreaterThan(0);
 });
 Deno.test("Route /wind", () => {
   expect(routes["/wind"].status).toBe(200);
+  expect(routes["/wind"].headers.get("content-length")).toBeGreaterThan(0);
 });
 Deno.test("Route /brow_noise", () => {
   expect(routes["/brow_noise"].status).toBe(200);
+  expect(routes["/brow_noise"].headers.get("content-length")).toBeGreaterThan(
+    0,
+  );
 });
 Deno.test("Route /leafs", () => {
   expect(routes["/leafs"].status).toBe(200);
+  expect(routes["/leafs"].headers.get("content-length")).toBeGreaterThan(0);
 });
 Deno.test("Route /drops", () => {
   expect(routes["/drops"].status).toBe(200);
+  expect(routes["/drops"].headers.get("content-length")).toBeGreaterThan(0);
 });
 Deno.test("Route /fire", () => {
   expect(routes["/fire"].status).toBe(200);
+  expect(routes["/fire"].headers.get("content-length")).toBeGreaterThan(0);
 });
 Deno.test("Route /night", () => {
   expect(routes["/night"].status).toBe(200);
+  expect(routes["/night"].headers.get("content-length")).toBeGreaterThan(0);
 });
 Deno.test("Route /train", () => {
   expect(routes["/train"].status).toBe(200);
+  expect(routes["/train"].headers.get("content-length")).toBeGreaterThan(0);
 });
 Deno.test("Route /fan", () => {
   expect(routes["/fan"].status).toBe(200);
+  expect(routes["/fan"].headers.get("content-length")).toBeGreaterThan(0);
 });
 Deno.test("Route /air_plane", () => {
   expect(routes["/air_plane"].status).toBe(200);
+  expect(routes["/air_plane"].headers.get("content-length")).toBeGreaterThan(0);
 });
 Deno.test("Route /underwater", () => {
   expect(routes["/underwater"].status).toBe(200);
+  expect(routes["/underwater"].headers.get("content-length")).toBeGreaterThan(
+    0,
+  );
 });
