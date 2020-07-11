e8040ea1df5868024cb760a474406935d78d6c2c
mateusfg7 <mateusfelipefg77@gmail.com>
2020-07-11 12:50:12 -0300

    update of audio filenames #2

--- modules/files.ts
+++ modules/files.ts
@@ -4,10 +4,10 @@ const sounds: IFiles = {
   rain: await Deno.readFile("files/sounds/rain.mp3"),
   water: await Deno.readFile("files/sounds/water.mp3"),
   storm: await Deno.readFile("files/sounds/storm.mp3"),
-  small_waves: await Deno.readFile("files/sounds/small_waves.mp3"),
-  ocean_waves: await Deno.readFile("files/sounds/ocean_waves.mp3"),
+  small_waves: await Deno.readFile("files/sounds/small-waves.mp3"),
+  ocean_waves: await Deno.readFile("files/sounds/ocean-waves.mp3"),
   forest_ambience: await Deno.readFile(
-    "files/sounds/forest_ambience.mp3",
+    "files/sounds/forest-ambience.mp3",
   ),
   coffee: await Deno.readFile(
     "files/sounds/coffee.mp3",
@@ -31,7 +31,7 @@ const sounds: IFiles = {
     "files/sounds/fan.mp3",
   ),
   air_plane: await Deno.readFile(
-    "files/sounds/air_plane.mp3",
+    "files/sounds/air-plane.mp3",
   ),
   underwater: await Deno.readFile(
     "files/sounds/underwater.mp3",
