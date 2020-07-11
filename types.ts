07d25d5e74800825a03caf453d885dfda7122d58
mateusfg7 <mateusfelipefg77@gmail.com>
2020-07-11 13:09:37 -0300

    pattern update of audio filenames in types file #2

--- types.ts
+++ types.ts
@@ -1,18 +1,18 @@
 export interface IFiles {
-  rain?: Uint8Array;
-  water?: Uint8Array;
-  storm?: Uint8Array;
-  small_waves?: Uint8Array;
-  ocean_waves?: Uint8Array;
-  forest_ambience?: Uint8Array;
-  coffee?: Uint8Array;
-  wind?: Uint8Array;
-  leafs?: Uint8Array;
-  drops?: Uint8Array;
-  fire?: Uint8Array;
-  night?: Uint8Array;
-  train?: Uint8Array;
-  fan?: Uint8Array;
-  air_plane?: Uint8Array;
-  underwater?: Uint8Array;
+  "rain"?: Uint8Array;
+  "water"?: Uint8Array;
+  "storm"?: Uint8Array;
+  "small-waves"?: Uint8Array;
+  "ocean-waves"?: Uint8Array;
+  "forest-ambience"?: Uint8Array;
+  "coffee"?: Uint8Array;
+  "wind"?: Uint8Array;
+  "leafs"?: Uint8Array;
+  "drops"?: Uint8Array;
+  "fire"?: Uint8Array;
+  "night"?: Uint8Array;
+  "train"?: Uint8Array;
+  "fan"?: Uint8Array;
+  "air-plane"?: Uint8Array;
+  "underwater"?: Uint8Array;
 }
